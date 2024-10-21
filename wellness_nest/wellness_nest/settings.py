bff7479fa0cf3cfdf0d4e808b2ebfe5913de4eeb
Saranya <jsaran.bj@gmail.com>
2024-10-21 15:19:51 +0200

    Add a secret key to settings.py

--- wellness_nest/wellness_nest/settings.py
+++ wellness_nest/wellness_nest/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z%f6+7j0i!!(6$*3!+$zdg$-bzhu$wa8+!!33v$a_dnj%a%oez'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'wellness_nest.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    
+
     'default':dj_database_url.parse(os.environ.get
     ("DATABASE_URL")),
     
