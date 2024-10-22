08844b3ac92e2046efef0566cfbab815b1ceed57
Saranya <jsaran.bj@gmail.com>
2024-10-22 15:01:59 +0200

    Created home app and create home page

--- wellness_nest/home/views.py
+++ wellness_nest/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """ Return to index page """
+    
+    return render(request, 'home/index.html')
--- wellness_nest/wellness_nest/settings.py
+++ wellness_nest/wellness_nest/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
 ]
 
@@ -64,7 +65,10 @@ ROOT_URLCONF = 'wellness_nest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -147,6 +151,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
