de13d1eaaaf04fb30187896e800e19282c9561c1
Saranya <jsaran.bj@gmail.com>
2024-10-30 09:59:11 +0100

    Load datas of fixtures

--- wellness_nest/products/models.py
+++ wellness_nest/products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    
+    """Model for Categories of the products"""
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    slug = models.SlugField(max_length=254, unique=True, null=True, blank=True)
+    
+    def __str__(self):
+        #print(self.name)
+        return self.name or ""
+
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+
+class Products(models.Model):
+    """ Model for all products fields """
+
+    category=models.ForeignKey('Category', null=True , blank=True, on_delete=models.SET_NULL)
+    sku=models.CharField(max_length=254,null=True,blank=True)
+    name=models.CharField(max_length=254)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+    rating=models.DecimalField(max_digits=6,decimal_places=2,null=True,blank=True)
+    image_url=models.URLField(max_length=1024,null=True,blank=True)
+    image=models.ImageField(null=True,blank=True)
+    size=models.CharField(max_length=50)
+    expected_dispatch=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
