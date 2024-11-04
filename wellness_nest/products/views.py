4a928dac0fe51c29fb0faf3d5a696f227e4a15c9
Saranya <jsaran.bj@gmail.com>
2024-11-04 11:15:27 +0100

    Add products to specific categories

--- wellness_nest/products/views.py
+++ wellness_nest/products/views.py
@@ -14,6 +14,16 @@ def all_products(request):
     categories=None
 
     if request.GET:
+        if 'category' in request.GET:
+            print(request.GET)
+            categories = request.GET['category'].split(',')
+            #print(categories)
+            #filters the products queryset to include only requested category
+            products= products.filter(category__name__in=categories)
+            #print(products)
+            #retrieves the Category object for requested category.
+            categories=Category.objects.filter(name__in=categories)
+
         if 'search_term' in request.GET:
             query = request.GET['search_term']
             if not query:
@@ -23,13 +33,7 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            print(categories)
-            products= products.filter(category__name__in=categories)
-            print(products)
-            categories=Category.objects.filter(name__in=categories)
-
+        
     context={
         'products' : products,
         'search_term':query,
