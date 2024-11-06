7a9f3a58a7a874c10db5274c3279cfb1c5fbee33
Saranya <jsaran.bj@gmail.com>
2024-11-06 12:02:38 +0100

    Add shopping bag functionality and contexts.py

--- wellness_nest/wellness_nest/settings.py
+++ wellness_nest/wellness_nest/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'shopping_bag.contexts.shopping_bag_contents',
             ],
         },
     },
@@ -159,6 +160,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD=50
+STANDARD_DELIVERY_PERCENTAGE=10
 
 
 # Default primary key field type
