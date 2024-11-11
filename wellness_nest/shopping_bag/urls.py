897b8573532b09cccf2c254218bfc74630fed798
Saranya <jsaran.bj@gmail.com>
2024-11-11 17:06:10 +0100

    Add Update functionality in shopping bag

--- wellness_nest/shopping_bag/urls.py
+++ wellness_nest/shopping_bag/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.shopping_bag_view, name='shopping_bag'),
-    path('add/<item_id>/', views.add_to_bag_view, name='add_to_bag')
+    path('add/<item_id>/', views.add_to_bag_view, name='add_to_bag'),
+    path('adjust/<item_id>/', views.adjust_bag_view, name='adjust_bag')
+
 ]
--- wellness_nest/shopping_bag/views.py
+++ wellness_nest/shopping_bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,reverse,HttpResponse,get_object_or_404
+from products.models import Products
+from django.contrib import messages
 
 # Create your views here.
 
@@ -22,3 +24,28 @@ def add_to_bag_view(request,item_id):#item_id=Product_id
     request.session['shopping_bag'] = shopping_bag
     print(request.session['shopping_bag'])
     return redirect(redirect_url)
+
+
+def adjust_bag_view(request,item_id):#item_id=Product_id
+    """adjust the quantity of specific product"""
+
+    product=get_object_or_404(Products,pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    shopping_bag = request.session.get('shopping_bag',{})#checks weather the user iniate adding item or bag id empty
+   
+    if quantity > 99:
+        messages.error(
+            request, 'Quantity must be less than or equal to 99.')
+    elif quantity > 0:
+        shopping_bag[item_id]=quantity
+        messages.success(
+            request, f'{product.name} quantity is added to {shopping_bag[item_id]}'
+        )
+    else:
+        shopping_bag.pop(item_id)
+        messages.success(request,f'Removed {product.name} from your shopping bag')
+        
+
+    request.session['shopping_bag'] = shopping_bag
+    print(request.session['shopping_bag'])
+    return redirect(reverse('shopping_bag'))
