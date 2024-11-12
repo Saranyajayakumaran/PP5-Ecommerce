8380e89e2cb5b3e188ced908e7c672df907855f2
Saranya <jsaran.bj@gmail.com>
2024-11-12 10:20:26 +0100

    Add bag_tools.py for subtotal of shopping bag

---
+++ wellness_nest/shopping_bag/templatetags/bag_tools.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+
+def calc_subtotal(price,quantity):
+    """ calculate subtotal of product and return it"""
+    return price * quantity
