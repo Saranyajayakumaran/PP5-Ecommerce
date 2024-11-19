7537b1fad6909704e03837ee3e1115cb149f7971
Saranya <jsaran.bj@gmail.com>
2024-11-19 11:55:05 +0100

    Update chaeckot form country placeholder

--- wellness_nest/payment/forms.py
+++ wellness_nest/payment/forms.py
@@ -21,16 +21,16 @@ class CheckoutForm(forms.ModelForm):
             'street_address2':'Street Address 2',
             'postcode':'Postal Code',
             'town_or_city':'Town or City',
-            'country':'Country',
         }
 
         self.fields['full_name'].widget.attrs['autofocus']=True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder']=placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder']=placeholder
             self.fields[field].widget.attrs['class']='srtipe-style-input'
             self.fields[field].label=False
 
