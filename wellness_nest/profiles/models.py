129396517d36d9c016898dda93d39f616dffbb65
Saranya <jsaran.bj@gmail.com>
2024-11-19 16:47:08 +0100

    Successfully login and log out

--- wellness_nest/profiles/models.py
+++ wellness_nest/profiles/models.py
@@ -32,5 +32,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
 
     if created:
         UserProfile.objects.create(user=instance)
-    # save the profile if existing users
+    #save the profile if existing users
     instance.userprofile.save()
