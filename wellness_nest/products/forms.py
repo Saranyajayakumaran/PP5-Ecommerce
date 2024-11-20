75a785cae884a50daabd08cbddd031a5f5be45de
Saranya <jsaran.bj@gmail.com>
2024-11-20 18:31:21 +0100

    Create a product form

--- wellness_nest/products/forms.py
+++ wellness_nest/products/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from .models import Products, Category
+
 
 class SortForm(forms.Form):
     SORT_OPTIONS = (
@@ -12,3 +14,19 @@ class SortForm(forms.Form):
     )
 
     sort = forms.ChoiceField(choices=SORT_OPTIONS, required=False)
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].chices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
