696cc7bf086bd9729a8b1d3ae7985ea054353785
Saranya <jsaran.bj@gmail.com>
2024-11-20 17:58:13 +0100

    Add confirmation email for order

--- wellness_nest/payment/webhook_handler.py
+++ wellness_nest/payment/webhook_handler.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+
+
 from .models import Checkout, CheckoutLineItem
 from products.models import Products
 from profiles.models import UserProfile
@@ -14,6 +18,22 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """Send the user a order confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'payment/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'payment/confirmation_emails/confirmation_email_body.txt',
+            {'order':order, 'contact_email':settings.DEFAULT_FROM_EMAIL})
+        send_mail (
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL
+            [cust_email]
+        )
 
     def handle_event(self, event):
         """
@@ -59,7 +79,7 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.save()
 
-        oder_exists = False
+        order_exists = False
         attempt = 1
         while attempt <= 5:
             try:
@@ -82,10 +102,10 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
-                logger.info(f"Order already exists: {order.order_number}")
-                return HttpResponse(
-                    content=f'Webhook received: {event["type"]}| SUCCESS: Verified order already in database',
-                    status=200) 
+            self._send_confirmation_email(order)   
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]}| SUCCESS: Verified order already in database',
+                status=200) 
         else:
             order = None  
             try:
@@ -112,14 +132,13 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         checkout_line_item.save() 
-                        logger.info(f"Order created successfully: {order.order_number}")
             except Exception as e:
                 if order:
                     order.delete()
-                    logger.error(f"Error processing payment intent: {e}")
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
-                    status=500)   
+                    status=500)
+        self._send_confirmation_email(order) 
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Creates order in webhook',
             status=200)
@@ -128,7 +147,6 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.payment_failed webhook from Stripe
         """
-        logger.info(f"Payment intent failed: {event['type']}")
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
