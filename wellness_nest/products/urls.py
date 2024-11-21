2aadc9625fe80117a8c110507dc71caa8bb379ca
Saranya <jsaran.bj@gmail.com>
2024-11-21 17:50:28 +0100

    Add edit product tempalte and view

--- wellness_nest/products/urls.py
+++ wellness_nest/products/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('products/<int:product_id>/',views.product_detail_view, name='product_detail'),
     path('products/category=<str:categories>/', views.all_products_view, name='products_by_category'),
     path('add/',views.add_product_view, name='add_product'),
-
+    path('edit/<int:product_id>/',views.edit_product_view, name='edit_product'),
 ]
