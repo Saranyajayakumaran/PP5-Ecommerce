6813c0de09760d873feaa0b0597ea8244c21c9ae
Saranya <jsaran.bj@gmail.com>
2024-11-21 18:30:27 +0100

    Edit and delete functionality to super user

--- wellness_nest/products/urls.py
+++ wellness_nest/products/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('products/category=<str:categories>/', views.all_products_view, name='products_by_category'),
     path('add/',views.add_product_view, name='add_product'),
     path('edit/<int:product_id>/',views.edit_product_view, name='edit_product'),
+    path('delete/<int:product_id>/',views.delete_product_view, name='delete_product'),
+
 ]
