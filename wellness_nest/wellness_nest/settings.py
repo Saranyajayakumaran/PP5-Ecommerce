36f1e908ad9be62d48db325a4201a38d3579b2c4
Saranya <jsaran.bj@gmail.com>
2024-11-22 15:18:02 +0100

    Changes for runtime deployment error

--- wellness_nest/wellness_nest/settings.py
+++ wellness_nest/wellness_nest/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pp5-wellnessnest-96adb2a059a0.herokuapp.com/','localhost','127.0.0.1',]
 CSRF_TRUSTED_ORIGINS = [
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
