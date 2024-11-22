c78991c9875595063dd475351b3dcf2b962566ab
Saranya <jsaran.bj@gmail.com>
2024-11-22 11:09:23 +0100

    Product admin - fix image firled

--- wellness_nest/products/urls.py
+++ wellness_nest/products/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products_view, name='products'),
-    path('products/<int:product_id>/',views.product_detail_view, name='product_detail'),
+    path('<int:product_id>/',views.product_detail_view, name='product_detail'),
     path('products/category=<str:categories>/', views.all_products_view, name='products_by_category'),
     path('add/',views.add_product_view, name='add_product'),
     path('edit/<int:product_id>/',views.edit_product_view, name='edit_product'),
--- wellness_nest/products/views.py
+++ wellness_nest/products/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 
-from django.contrib.auth.decorators import login_required
 
 from django.db.models.functions import Lower
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from.models import Products, Category
 from .forms import ProductForm
