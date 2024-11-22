095c7cdf19d22c45c280079d1686223115bb3372
Saranya <jsaran.bj@gmail.com>
2024-11-22 15:21:21 +0100

    Set debug to true only when development

--- wellness_nest/wellness_nest/settings.py
+++ wellness_nest/wellness_nest/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY",'')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT'in os.environ
 
 ALLOWED_HOSTS = ['pp5-wellnessnest-96adb2a059a0.herokuapp.com/','localhost','127.0.0.1',]
 CSRF_TRUSTED_ORIGINS = [
