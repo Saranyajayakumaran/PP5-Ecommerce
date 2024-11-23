d65d58dc59cc56c762ee1a531e53d304dcaeda12
Saranya <jsaran.bj@gmail.com>
2024-11-23 07:58:48 +0100

    change access key for aws s3 storage

--- wellness_nest/wellness_nest_project/settings.py
+++ wellness_nest/wellness_nest_project/settings.py
@@ -200,7 +200,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'pp5-wellness-nest'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
