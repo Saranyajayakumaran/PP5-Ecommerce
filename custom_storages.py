10ed9d77ff7c2b769832b197172fb90a5cecbd5f
Saranya <jsaran.bj@gmail.com>
2024-11-23 19:25:38 +0100

    Change ststic root directory

--- custom_storages.py
+++ custom_storages.py
@@ -6,11 +6,10 @@ class StaticStorage(S3Boto3Storage):
     Custom storage for static files, stored in a specific S3 location.
     """
     location = settings.STATICFILES_LOCATION
-    file_overwrite = False  # Optional: Ensures that files are not overwritten if the same file name exists
-
+    
 class MediaStorage(S3Boto3Storage):
     """
     Custom storage for media files, stored in a specific S3 location.
     """
     location = settings.MEDIAFILES_LOCATION
-    file_overwrite = False  # Optional: Ensures that files are not overwritten if the same file name exists
+    
