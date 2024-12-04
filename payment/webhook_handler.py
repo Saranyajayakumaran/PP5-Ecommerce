eef1c9b7570c5f4c71c19a36f7a3bc6826ae25ba
Saranya <jsaran.bj@gmail.com>
2024-12-04 14:51:34 +0100

    signup and login form style

--- payment/webhook_handler.py
+++ payment/webhook_handler.py
@@ -22,6 +22,8 @@ class StripeWH_Handler:
 
     def _send_confirmation_email(self, order):
         """Send the user a order confirmation email"""
+
+        print(f"Email is sending {order.email}")
         cust_email = order.email
         subject = render_to_string('payment/confirmation_emails/confirmation_email_subject.txt',{'order': order})
         body = render_to_string('payment/confirmation_emails/confirmation_email_body.txt',{'order':order, 'contact_email':settings.DEFAULT_FROM_EMAIL})
